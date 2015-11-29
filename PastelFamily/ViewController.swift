6ce7ce1c67c1ab4e5d6514d54cd8c1c766df82e9
Koichiro Oishi <bs@v7.com>
2015-11-29 21:42:38 +0900

    add TableView

--- PastelFamily/ViewController.swift
+++ PastelFamily/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource
+{
 
-  override func viewDidLoad() {
+  @IBOutlet weak var tableView: UITableView!
+
+  override func viewDidLoad()
+  {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    
+    tableView.dataSource = self
+    
   }
 
-  override func didReceiveMemoryWarning() {
+  override func didReceiveMemoryWarning()
+  {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
 
+// MARK: - UITableViewDataSource
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+  {
+  }
 
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+  {
+    return 1
+  }
+
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int
+  {
+    return 1
+  }
+  
 }
-
