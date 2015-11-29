668055e5bf05ee10ed94396a50eb27ff1a10bd0a
Koichiro Oishi <bs@v7.com>
2015-11-29 22:04:20 +0900

    add cell

---
+++ PastelFamily/CustomTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTableViewCell.swift
+//  PastelFamily
+//
+//  Created by bs on 2015/11/29.
+//  Copyright © 2015年 bs. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell
+{
+
+  @IBOutlet weak var komaImage: UIImageView!
+  
+  override func awakeFromNib()
+  {
+    super.awakeFromNib()
+  }
+
+  override func setSelected(selected: Bool, animated: Bool)
+  {
+    super.setSelected(selected, animated: animated)
+  }
+
+}
